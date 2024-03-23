492f0a57a7fc46dc2324fcd7d6e2de134fd32e0b
RomaLzhih <791191656@qq.com>
2024-03-22 18:44:40 -0700

    update wezterm config

--- .dotfyles/.wezterm.lua
+++ .dotfyles/.wezterm.lua
@@ -11,58 +11,56 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Rosé Pine (base16)"
+
 -- config.color_scheme = "Gruvbox Dark (Gogh)"
--- config.color_scheme = "Tokyo Night"
-
--- config.font = wezterm.font("Maple Mono NF")
--- config.font_size = 11.5
--- config.line_height = 0.95
-
--- config.font = wezterm.font({
--- 	family = "CartographCF Nerd Font",
--- 	harfbuzz_features = {
--- 		"liga",
--- 		"ss02",
--- 	},
--- })
--- config.font_size = 12
--- config.line_height = 1
+-- config.color_scheme = "Rosé Pine (base16)"
+config.color_scheme = "Pomimandres"
 
 config.font = wezterm.font({
-	family = "Carto Lisa",
-	-- family = "Mono Lisa Nerd Font",
-	-- family = "JetBrains Mono Nerd Font",
+	-- family = "Carto Lisa",
+	-- family = "JetBrainsMono NF",
+	family = "MonoLisa Nerd Font",
 	harfbuzz_features = {
 		"liga",
 		"ss02",
+		-- "ss04", -- alter g
+		"ss07",
+		"ss11",
+		"ss12",
+		"ss14",
 	},
 })
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			weight = "Bold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Half",
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			weight = "DemiBold",
+			style = "Italic",
+		}),
+	},
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font({
+			family = "VictorMono Nerd Font",
+			weight = "DemiBold",
+			style = "Italic",
+		}),
+	},
+}
 config.font_size = 11
-config.line_height = 1
 
--- config.font = wezterm.font({
--- 	family = "MonoLisa Nerd Font",
--- 	harfbuzz_features = {
--- 		"liga",
--- 		"ss01",
--- 		"ss02",
--- 		-- "ss03", -- alter g
--- 		"ss07",
--- 		"ss11",
--- 		"ss12",
--- 		"ss14",
--- 	},
--- })
--- config.font_size = 11
--- config.line_height = 1
-
--- config.font = wezterm.font("DankMono Nerd Font")
--- config.font_size = 12.5
--- config.font = wezterm.font("SFMono Nerd Font Mono")
--- config.font = wezterm.font("SauceCodePro Nerd Font")
--- config.font = wezterm.font("Recursive")
---
 config.animation_fps = 60
 config.default_cursor_style = "BlinkingBlock"
 config.max_fps = 120
