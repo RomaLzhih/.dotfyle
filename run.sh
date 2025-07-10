8770f1395b695402032ee01557bc354bbf31a80c
Swans&Swan <791191656@qq.com>
2025-07-10 13:49:01 -0400

    update zsh

--- run.sh
+++ run.sh
@@ -49,12 +49,8 @@ if [[ ${kUpdate} == 1 ]]; then
 
     # NOTE: COPY file
     rsync -r --no-perms --no-owner --include="*/" --include=".*" "dotfyles/" "${HOME}/"
-    if [[ ${os} == "ubuntu" ]]; then
-        # git clone --single-branch https://github.com/gpakosz/.tmux.git
-        ln -s -f ${HOME}/.tmux/.tmux.conf ${HOME}/.tmux.conf
-    else
-        rm ${HOME}/.tmux.conf.local
-    fi
+    # git clone --single-branch https://github.com/gpakosz/.tmux.git
+    ln -s -f ${HOME}/.tmux/.tmux.conf ${HOME}/.tmux.conf
 
     # NOTE: neovim
     if [[ ${kUpdateVim} == 1 ]] && [[ "$OSTYPE" != "darwin"* ]]; then
